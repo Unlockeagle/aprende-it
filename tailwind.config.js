b966f01b99dbe24c77d51d63c3665dae519c4ae5
Yoan <135377548+Unlockeagle@users.noreply.github.com>
2024-03-10 12:54:38 -0400

    se arreglo carpeta public

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./public/**/*.html", "./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./public/**/*.{html, js}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
