bc50867b89124d7afc3d7bde8f6f36822c3d63bc
Yoan <135377548+Unlockeagle@users.noreply.github.com>
2024-03-10 06:31:31 -0400

    se cambio configuracion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./public/**/*.html", "./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
