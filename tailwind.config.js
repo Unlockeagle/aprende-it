70da7546429573ff2f0a02d7378753e4f8ddb8ba
Yoan <135377548+Unlockeagle@users.noreply.github.com>
2024-03-10 13:13:38 -0400

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./public/**/*.{html, js}",
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./public/**/*.{html, js}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
